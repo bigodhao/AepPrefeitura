905eb58f289c309ba2fe92d035e633bdab4c7a57
João Carvalho <jpaulofranca10@gmail.com>
2021-05-23 17:35:55 -0300

    criação do projeto Java

---
+++ ProjetoConteudoProfessores/src/main/java/com/projeto/ProjetoConteudoProfessores/ProjetoConteudoProfessoresApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ProjetoConteudoProfessores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConteudoProfessoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoConteudoProfessoresApplication.class, args);
+	}
+
+}
---
+++ ProjetoConteudoProfessores/src/test/java/com/projeto/ProjetoConteudoProfessores/ProjetoConteudoProfessoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ProjetoConteudoProfessores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoConteudoProfessoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
